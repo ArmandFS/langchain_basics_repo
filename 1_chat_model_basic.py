c56b3cbec2c38993570071808301d4f9cf6439b4
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2025-08-17 23:05:05 +1200

    Add files via upload

---
+++ 1_chat_model_basic.py
@@ -0,0 +1,20 @@
+# Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/
+# OpenAI Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/openai/
+
+from dotenv import load_dotenv
+from langchain_community.chat_models import ChatOllama
+# Load environment variables from .env
+load_dotenv()
+
+# Create a ChatOpenAI model
+model = ChatOllama(model="llama3")
+
+# Invoke the model with a message
+result = model.invoke("What is 81 divided by 9?")
+print("Full result:")
+print(result)
+print("Content only:")
+print(result.content)
+
+
+
